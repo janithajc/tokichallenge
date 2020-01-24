60e4f3334acf77a40a3e5b61b4d4814d69e1696d
Janitha Samarasinghe <Samarasinghe.Janitha@corp.sysco.com>
2020-01-24 21:16:07 +0530

    - Injecting property value to BASE_URL

--- src/main/java/com/janitha/tokichallenge/service/HttpServiceImpl.java
+++ src/main/java/com/janitha/tokichallenge/service/HttpServiceImpl.java
@@ -1,5 +1,6 @@
 package com.janitha.tokichallenge.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ClientResponse;
@@ -8,10 +9,12 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class HttpServiceImpl implements HttpService {
-//  @Value("${api.baseURL}")
-  private String BASE_URL = "https://tokigames-challenge.herokuapp.com/api/flights";
 
-  private final WebClient webClient = WebClient.create(BASE_URL);
+  private final WebClient webClient;
+
+  public HttpServiceImpl(@Value("${api.baseURL}") final String BASE_URL) {
+    webClient = WebClient.create(BASE_URL);
+  }
 
   @Override
   public Mono<ClientResponse> get(String uri) {
