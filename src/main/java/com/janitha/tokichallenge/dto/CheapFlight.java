fcffd826f3531c738746242722602faff1927d42
Janitha Samarasinghe <Samarasinghe.Janitha@corp.sysco.com>
2020-01-25 17:29:56 +0530

    - Fixed JSON error on

--- src/main/java/com/janitha/tokichallenge/dto/CheapFlight.java
+++ src/main/java/com/janitha/tokichallenge/dto/CheapFlight.java
@@ -2,11 +2,13 @@ package com.janitha.tokichallenge.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class CheapFlight {
   private String route;
   private Long departure;
